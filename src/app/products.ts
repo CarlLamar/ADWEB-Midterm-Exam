21adb0c2b0aebdd785480b689ad5959f6b66c643
CarlLamar <100596478+CarlLamar@users.noreply.github.com>
2023-03-24 21:16:51 +0800

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -9,27 +9,27 @@ export interface Product {
 export const products: Product[] = [
   {
     id: 1,
-    name: 'LAMBORGHINI',
+    name: 'Lamborghini',
     price: 799,
     description:
       "We are Lamborghini specialists with factory qualified expertise of a broad variety of Lamborghini models, whether you require Huracan repair or wish to tune your Aventador. We've gained expert understanding of a wide range of Lamborghini models, so we can service a Diablo or a brand-new Urus. Our expert staff of factory-trained technicians has been working on Lamborghini automobiles for decades, acquiring vital expertise that no manufacturer or factory-owned dealership can match. Our service center is equipped with genuine Lamborghini diagnostic equipment and factory tools, allowing us to deliver the high-quality services that your Lamborghini deserves.",
-    parts: 'Engine, Tires',
+    parts: 'Certified Repairs, Classic Spare Parts & Original Spare Parts',
   },
   {
     id: 2,
-    name: 'FERRARI',
+    name: 'Scuderia Ferrari',
     price: 699,
     description:
       "We can maintain your Ferrari or lower it on factory suspension, and we can give high-quality servicing for your Italian supercar. We've worked on the 360, 430, 458, F8, and 812 models. Our professionals have performed everything from factory level maintenance to engine and suspension component modification.",
-    parts: 'Engine, Tires, Doors',
+    parts: 'Labour, Original Replacement Parts, Lubricants, Engine Oil, Brake Fluid.',
   },
   {
     id: 3,
-    name: 'MCLAREN',
+    name: 'McLaren',
     price: 299,
     description:
       'Our expert technicians have worked on a variety of McLaren automobiles, earning knowledge with models like as the 12C, 570S, 720S, and 765LT. From servicing to personalization, we have considerable understanding with McLaren automobiles. We have the knowledge and experience to operate on McLaren cars and offer a high-quality option for servicing and maintenance.',
-    parts: 'Engine, Tires, Headlight',
+    parts: 'Repairs, Labour & Original Replacement Parts',
   },
 ];
 
